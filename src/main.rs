c1bcb1111e07d9260bc43006b2f97e4fe9ce6910
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-12-24 07:37:16 +0900

    header and table background

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,63 @@
 use svg;
+use svg::node::element;
 
 fn main() {
-    let rect = svg::node::element::Rectangle::new()
-        .set("x", 50)
-        .set("y", 80)
-        .set("width", 300)
-        .set("height", 36);
+    let width = 300;
+    let x = 50;
+    let y = 80;
+    let table_height = 247;
+    let header_height = 36;
+    let border_radius = 6;
+    let light_gray_color = "#494949";
 
-    let clip_path = svg::node::element::ClipPath::new()
-        .set("id", "cut-off-table-header")
+    // clip path
+    let rect = element::Rectangle::new()
+        .set("x", x)
+        .set("y", y)
+        .set("width", width)
+        .set("height", header_height);
+
+    let header_clip_path_id = "cut-off-table-header";
+    let clip_path = element::ClipPath::new()
+        .set("id", header_clip_path_id)
         .add(rect);
 
-    let defs = svg::node::element::Definitions::new().add(clip_path);
+    let header_bg = element::Rectangle::new()
+        .set("x", x)
+        .set("y", y)
+        .set("width", width)
+        .set("height", table_height)
+        .set("rx", border_radius)
+        .set("ry", border_radius)
+        .set("stroke", light_gray_color)
+        .set("fill", light_gray_color)
+        .set("clip-path", format!("url(#{})", header_clip_path_id));
 
-    let doc = svg::Document::new().set("version", "1.1").add(defs);
+    // Background
+    let background = element::Rectangle::new()
+        .set("width", "100%")
+        .set("height", "100%")
+        .set("fill", "#1c1c1c");
+
+    // Table
+    let table_bg = element::Rectangle::new()
+        .set("x", x)
+        .set("y", y)
+        .set("width", width)
+        .set("height", 247)
+        .set("rx", border_radius)
+        .set("ry", border_radius)
+        .set("stroke", light_gray_color)
+        .set("fill", "#212121");
+
+    // doc
+    let defs = element::Definitions::new();
+    let doc = svg::Document::new()
+        .set("version", "1.1")
+        .add(defs.add(clip_path))
+        .add(background)
+        .add(table_bg)
+        .add(header_bg);
 
     println!("{}", doc);
 }
