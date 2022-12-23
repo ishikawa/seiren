de24c8975b1a69493ca02ccff945e0349c7e030e
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-12-24 08:00:39 +0900

    draw rows

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ fn main() {
     let header_height = 36;
     let border_radius = 6;
     let light_gray_color = "#494949";
+    let text_color = "white";
+    let header_text = "users";
+    let rows = ["id", "uuid"];
 
     // clip path
     let rect = element::Rectangle::new()
@@ -33,6 +36,14 @@ fn main() {
         .set("fill", light_gray_color)
         .set("clip-path", format!("url(#{})", header_clip_path_id));
 
+    let header_text = element::Text::new()
+        .set("x", x + 12)
+        .set("y", y + 23)
+        .set("fill", text_color)
+        .set("font-weight", "bold")
+        .set("font-family", "Monaco,monospace")
+        .add(svg::node::Text::new(header_text));
+
     // Background
     let background = element::Rectangle::new()
         .set("width", "100%")
@@ -52,12 +63,39 @@ fn main() {
 
     // doc
     let defs = element::Definitions::new();
-    let doc = svg::Document::new()
+    let mut doc = svg::Document::new()
         .set("version", "1.1")
         .add(defs.add(clip_path))
         .add(background)
         .add(table_bg)
-        .add(header_bg);
+        .add(header_bg)
+        .add(header_text);
+
+    // rows
+    let base = y + header_height;
+
+    for (i, row) in rows.iter().enumerate() {
+        if i > 0 {
+            let y = base + 34 * i;
+            let line = element::Line::new()
+                .set("x1", x)
+                .set("x2", x + width)
+                .set("y1", y)
+                .set("y2", y)
+                .set("stroke", light_gray_color)
+                .set("stroke-width", 1);
+            doc = doc.add(line);
+        }
+
+        let label = element::Text::new()
+            .set("x", x + 12)
+            .set("y", (base + 22) + 35 * i)
+            .set("fill", text_color)
+            .set("font-weight", "lighter")
+            .set("font-family", "Courier New,monospace")
+            .add(svg::node::Text::new(*row));
+        doc = doc.add(label);
+    }
 
     println!("{}", doc);
 }
