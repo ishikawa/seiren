cd8fd227ef1bc42550929ee613d3b6151e1b0985
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-12-24 06:48:07 +0900

    add svg lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use svg;
+
 fn main() {
-    println!("Hello, world!");
+    let rect = svg::node::element::Rectangle::new()
+        .set("x", 50)
+        .set("y", 80)
+        .set("width", 300)
+        .set("height", 36);
+
+    let clip_path = svg::node::element::ClipPath::new()
+        .set("id", "cut-off-table-header")
+        .add(rect);
+
+    let defs = svg::node::element::Definitions::new().add(clip_path);
+
+    let doc = svg::Document::new().set("version", "1.1").add(defs);
+
+    println!("{}", doc);
 }
