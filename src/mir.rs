be4dcc1f7a352449558afd02161900120ed6a71f
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2022-12-25 11:09:37 +0900

    Node and NodeKind

--- src/mir.rs
+++ src/mir.rs
@@ -12,38 +12,102 @@
 //! ```
 use crate::color::WebColor;
 use derive_builder::Builder;
+use std::fmt;
+
+/// https://developer.mozilla.org/en-US/docs/Web/SVG/Content_type#length
+///
+/// length used in SVG:
+/// ```
+/// length ::= number ("em" | "ex" | "px" | "in" | "cm" | "mm" | "pt" | "pc" | "%")?
+/// ```
+#[derive(Debug, Clone, Copy, Default, PartialEq)]
+pub struct Length {
+    pub value: f32,
+    pub unit: Option<LengthUnit>,
+}
+
+impl fmt::Display for Length {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:.1}", self.value)?;
+        let Some(unit) = self.unit else { return Ok(()) };
+        write!(f, "{}", unit)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum LengthUnit {
+    Pixel,
+    Percentage,
+}
+
+impl fmt::Display for LengthUnit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LengthUnit::Pixel => write!(f, "px"),
+            LengthUnit::Percentage => write!(f, "%"),
+        }
+    }
+}
 
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct Point {
-    pub x: f32,
-    pub y: f32,
+    pub x: Length,
+    pub y: Length,
 }
 
 impl Point {
-    pub fn new(x: f32, y: f32) -> Self {
+    pub fn new(x: Length, y: Length) -> Self {
         Self { x, y }
     }
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct Size {
-    pub width: f32,
-    pub height: f32,
+    pub width: Length,
+    pub height: Length,
 }
 
 impl Size {
-    pub fn new(width: f32, height: f32) -> Self {
+    pub fn new(width: Length, height: Length) -> Self {
         Self { width, height }
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct NodeId(usize);
+
+impl fmt::Display for NodeId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 #[derive(Debug)]
-pub enum Node {
+pub struct Node {
+    pub id: NodeId,
+    pub parent_node_id: Option<NodeId>,
+    pub origin: Option<Point>,
+    pub size: Option<Size>,
+    pub kind: NodeKind,
+}
+
+impl Node {
+    pub fn new(id: NodeId, kind: NodeKind) -> Self {
+        Self {
+            id,
+            kind,
+            parent_node_id: None,
+            origin: None,
+            size: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum NodeKind {
     Record(RecordNode),
     Field(FieldNode),
 }
-
-pub struct NodeId(usize);
 
 #[derive(Debug, Default)]
 pub struct Document {
@@ -55,6 +119,8 @@ impl Document {
         Self::default()
     }
 
+    // -- Get a node
+
     pub fn get_node(&self, node_id: &NodeId) -> Option<&Node> {
         self.nodes.get(node_id.0)
     }
@@ -63,18 +129,24 @@ impl Document {
         self.nodes.get_mut(node_id.0)
     }
 
-    pub fn add_record(&mut self, field: RecordNode) -> NodeId {
-        let index = self.nodes.len();
+    // -- Create a node
 
-        self.nodes.push(Node::Record(field));
-        NodeId(index)
+    pub fn create_record(&mut self, record: RecordNode) -> NodeId {
+        let index = self.nodes.len();
+        let node_id = NodeId(index);
+        let node = Node::new(node_id, NodeKind::Record(record));
+
+        self.nodes.push(node);
+        node_id
     }
 
-    pub fn add_field(&mut self, field: FieldNode) -> NodeId {
+    pub fn create_field(&mut self, field: FieldNode) -> NodeId {
         let index = self.nodes.len();
+        let node_id = NodeId(index);
+        let node = Node::new(node_id, NodeKind::Field(field));
 
-        self.nodes.push(Node::Field(field));
-        NodeId(index)
+        self.nodes.push(node);
+        node_id
     }
 }
 
@@ -126,26 +198,26 @@ mod tests {
 
         let field = FieldNodeBuilder::default().name("id").build().unwrap();
 
-        let node_id = doc.add_field(field);
+        let node_id = doc.create_field(field);
 
         let node = doc.get_node(&node_id);
-
         assert!(node.is_some());
 
-        let Node::Field(field) = node.unwrap() else { panic!() };
+        let node = node.unwrap();
+        let NodeKind::Field(field) = &node.kind else { panic!() };
 
         assert_eq!(field.name, "id");
 
         // mutate
         {
-            let node = doc.get_node_mut(&node_id);
-            let Node::Field(field) = node.unwrap() else { panic!() };
+            let node = doc.get_node_mut(&node_id).unwrap();
+            let NodeKind::Field(field) = &mut node.kind else { panic!() };
 
             field.name = "uuid".to_string();
         }
 
-        let node = doc.get_node(&node_id);
-        let Node::Field(field) = node.unwrap() else { panic!() };
+        let node = doc.get_node(&node_id).unwrap();
+        let NodeKind::Field(field) = &node.kind else { panic!() };
 
         assert_eq!(field.name, "uuid");
     }
