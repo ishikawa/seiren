b29aa869edb08209912bcc647d92b45390211635
Takanori Ishikawa <ishikawa.takanori@abejainc.com>
2022-12-27 15:22:50 +0900

    execute_connection_point_layout

--- src/layout.rs
+++ src/layout.rs
@@ -63,5 +63,70 @@ impl LayoutEngine for SimpleLayoutEngine {
         }
     }
 
-    fn execute_connection_point_layout(&self, doc: &mut mir::Document) {}
+    fn execute_connection_point_layout(&self, doc: &mut mir::Document) {
+        let child_id_vec = doc.body().children().collect::<Vec<_>>();
+
+        for (_, child_id) in child_id_vec.iter().enumerate() {
+            let Some(record_node) = doc.get_node_mut(child_id) else { continue };
+            let Some(record_rect) = record_node.rect() else { continue };
+
+            // In the case of a rectangle, connection points are placed in
+            // the center of each of the four edges.
+            for pt in [
+                Point::new(record_rect.mid_x(), record_rect.min_y()), // top
+                Point::new(record_rect.max_x(), record_rect.mid_y()), // right
+                Point::new(record_rect.mid_x(), record_rect.max_y()), // bottom
+                Point::new(record_rect.min_x(), record_rect.mid_y()), // left
+            ] {
+                record_node.append_connection_point(pt);
+            }
+
+            // For each field in a rectangle, connection points are placed
+            // the center of:
+            // - each of the four edges - if the number of fields is `1`.
+            // - top, left and right - for the top field
+            // - bottom, left and right - for the bottom field
+            // - left and right - for the rest
+            let field_id_vec = record_node.children().collect::<Vec<_>>();
+
+            for (field_index, field_node_id) in field_id_vec.iter().enumerate() {
+                let Some(field_node) = doc.get_node_mut(field_node_id) else { continue };
+                let Some(field_rect) = field_node.rect() else { continue };
+
+                if field_id_vec.len() == 1 {
+                    for pt in [
+                        Point::new(field_rect.mid_x(), field_rect.min_y()), // top
+                        Point::new(field_rect.max_x(), field_rect.mid_y()), // right
+                        Point::new(field_rect.mid_x(), field_rect.max_y()), // bottom
+                        Point::new(field_rect.min_x(), field_rect.mid_y()), // left
+                    ] {
+                        field_node.append_connection_point(pt);
+                    }
+                } else if field_index == 0 {
+                    for pt in [
+                        Point::new(field_rect.mid_x(), field_rect.min_y()), // top
+                        Point::new(field_rect.max_x(), field_rect.mid_y()), // right
+                        Point::new(field_rect.min_x(), field_rect.mid_y()), // left
+                    ] {
+                        field_node.append_connection_point(pt);
+                    }
+                } else if field_index == (field_id_vec.len() - 1) {
+                    for pt in [
+                        Point::new(field_rect.max_x(), field_rect.mid_y()), // right
+                        Point::new(field_rect.mid_x(), field_rect.max_y()), // bottom
+                        Point::new(field_rect.min_x(), field_rect.mid_y()), // left
+                    ] {
+                        field_node.append_connection_point(pt);
+                    }
+                } else {
+                    for pt in [
+                        Point::new(field_rect.max_x(), field_rect.mid_y()), // right
+                        Point::new(field_rect.min_x(), field_rect.mid_y()), // left
+                    ] {
+                        field_node.append_connection_point(pt);
+                    }
+                }
+            }
+        }
+    }
 }
