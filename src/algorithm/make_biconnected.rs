9106576f20fe867c6ec9facecedbbe34bbbcf7b8
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2023-01-16 23:12:24 +0900

    add tests

--- src/algorithm/make_biconnected.rs
+++ src/algorithm/make_biconnected.rs
@@ -105,4 +105,38 @@ mod tests {
         assert_eq!(low_link.articulations.len(), 0);
         assert_eq!(low_link.bridges.len(), 0);
     }
+
+    #[test]
+    fn test_make_biconnected_3() {
+        // ```ignore
+        //     6
+        //     |
+        //  0--1--2--3
+        //     |
+        //     4
+        //     |
+        //     5
+        // ```
+        let mut g: UnGraph<&str, &str> = UnGraph::<&str, &str>::default();
+
+        let v0 = g.add_node("v0");
+        let v1 = g.add_node("v1");
+        let v2 = g.add_node("v2");
+        let v3 = g.add_node("v3");
+        let v4 = g.add_node("v4");
+        let v5 = g.add_node("v5");
+        let v6 = g.add_node("v6");
+
+        g.extend_with_edges(&[(v0, v1), (v1, v2), (v2, v3), (v1, v4), (v4, v5), (v1, v6)]);
+
+        // -- convert it to biconnected graph
+        make_biconnected(&mut g);
+
+        let mut low_link = LowLink::new(&g);
+
+        low_link.traverse(&g);
+
+        assert_eq!(low_link.articulations.len(), 0);
+        assert_eq!(low_link.bridges.len(), 0);
+    }
 }
